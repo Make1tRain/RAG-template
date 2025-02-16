d71337b947fba7e61f5a7e916b8e4452dd7d1d0d
Make1tRain <asilberkekoroglu@gmail.com>
2025-02-16 18:54:09 +0100

    added CSV functionality

--- src/functions/pdf_processing.py
+++ src/functions/pdf_processing.py
@@ -1,11 +1,13 @@
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders.csv_loader import CSVLoader
 from typing import List
 from langchain.schema.document import Document
 
-### PDF DOCUMENT PROCESSING FUNCTIONS ###
+### DOCUMENT PROCESSING FUNCTIONS ###
 
-def load_documents(file_path: str) -> List[Document]:
+
+def load_pdf(file_path: str) -> List[Document]:
     """
     Loads PDF documents from the specified path.
 
@@ -18,8 +20,38 @@ def load_documents(file_path: str) -> List[Document]:
     loader = PyPDFDirectoryLoader(file_path)
     return loader.load()
 
+
+def load_csv(file_path: str) -> List[Document]:
+    """
+    Loads CSV documents from the specified path.
+
+    Args:
+        file_path (str): Path to a single CSV file or a directory containing multiple CSVs.
+
+    Returns:
+        List[Document]: A list of Document objects extracted from CSVs.
+    """
+    loader = CSVLoader(file_path=file_path)
+    return loader.load()
+
+
+def load_documents(file_path: str) -> List[Document]:
+
+    documents = []
+    for document in load_pdf(file_path):
+        documents.append(document)
+
+    for document in load_csv(file_path):
+        documents.append(document)
+
+    return documents
+
+
 def split_documents(
-    documents: List[Document], chunk_size: int = 800, chunk_overlap: int = 80, length_function=len
+    documents: List[Document],
+    chunk_size: int = 800,
+    chunk_overlap: int = 80,
+    length_function=len,
 ) -> List[Document]:
     """
     Splits a list of Document objects into smaller, overlapping chunks for easier processing.
@@ -34,6 +66,8 @@ def split_documents(
         List[Document]: A list of smaller chunked Document objects.
     """
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=chunk_size, chunk_overlap=chunk_overlap, length_function=length_function
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap,
+        length_function=length_function,
     )
     return text_splitter.split_documents(documents)
